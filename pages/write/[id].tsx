1f14691cff77fd4598cd20d95ae96a58ba978f09
Igor Strelnyk <59284261+rodriguezz4060@users.noreply.github.com>
2024-04-23 10:04:31 +0300

    Update [id].tsx

--- pages/write/[id].tsx
+++ pages/write/[id].tsx
@@ -1,4 +1,4 @@
-import { NextPage } from 'next'
+import { NextPage, GetServerSideProps } from 'next'
 import React from 'react'
 import { MainLayout } from '../../layouts/MainLayout'
 import { WriteForm } from '../../components/WriteForm'
@@ -6,48 +6,58 @@ import { Api } from '../../utils/api'
 import { PostItem } from '../../utils/api/types'
 
 interface WritePageProps {
-  post: PostItem
+	post: PostItem
 }
 
 const WritePage: NextPage<WritePageProps> = ({ post }) => {
-  return (
-    <MainLayout className='main-layout-white' hideComments hideMenu>
-      <WriteForm data={post} />
-    </MainLayout>
-  )
+	return (
+		<MainLayout className='main-layout-white' hideComments hideMenu>
+			<WriteForm data={post} />
+		</MainLayout>
+	)
 }
 
-export const getServerSideProps: getServerSideProps = async ctx => {
-  try {
-    const id = ctx.params.id
-    const post = await Api(ctx).post.getOne(+id)
-    const user = await Api(ctx).user.getMe()
+export const getServerSideProps: GetServerSideProps = async ctx => {
+	try {
+		const id = ctx.params?.id
 
-    if (post.user.id !== user.id) {
-      return {
-        props: {},
-        redirect: {
-          destination: '/',
-          permanent: false,
-        },
-      }
-    }
+		if (!id) {
+			return {
+				redirect: {
+					destination: '/',
+					permanent: false
+				}
+			}
+		}
 
-    return {
-      props: {
-        post,
-      },
-    }
-  } catch (err) {
-    console.log('Write page', err)
-    return {
-      props: {},
-      redirect: {
-        destination: '/',
-        permanent: false,
-      },
-    }
-  }
+		const post = await Api(ctx).post.getOne(+id)
+		const user = await Api(ctx).user.getMe()
+
+		if (post.user.id !== user.id) {
+			return {
+				props: {},
+				redirect: {
+					destination: '/',
+					permanent: false
+				}
+			}
+		}
+
+		return {
+			props: {
+				post
+			}
+		}
+	} catch (err) {
+		console.log('Write page', err)
+		return {
+			props: {},
+			redirect: {
+				destination: '/',
+				permanent: false
+			}
+		}
+	}
 }
 
 export default WritePage
